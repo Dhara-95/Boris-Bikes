7da5228989de0b3232c5c9d7d8e6c14462b64c00
Dhara <dhara.patel95@hotmail.co.uk>
2020-06-14 11:05:04 +0100

    Step 13 incomplete

--- Boris-Bikes/lib/docking_station.rb
+++ Boris-Bikes/lib/docking_station.rb
@@ -3,12 +3,17 @@ require_relative 'bike.rb'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bike = bike
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
   end
-  
+
   def dock(bike)
+    fail 'At full capacity' if @bike
     @bike = bike
   end
 end
--- Boris-Bikes/spec/docking_station_spec.rb
+++ Boris-Bikes/spec/docking_station_spec.rb
@@ -1,13 +1,18 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  describe '#release_bike' do
+    it { is_expected.to respond_to :release_bike }
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
   it 'returns docked bike' do
@@ -16,17 +21,17 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
-  describe '#release_bike' do
-    it 'releases a bike' do
+  it 'docks something' do
     bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
-    end
+    expect(subject.dock(bike)).to eq bike
   end
 
-  describe '#release_bike' do
-    it 'raies an error when there are no bikes available' do
-    expect {subject.release_bike}.to raise_error 'No bikes available'
+  it { is_expected.to respond_to(:bike) }
+
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it 'raises an error when there is already a bike in docking station' do
+      expect {subject.dock Bike.new}.to raise_error 'At full capacity'
     end
   end
 end
